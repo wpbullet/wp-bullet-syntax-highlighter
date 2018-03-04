737d06059c23d71f56304adf5ce5a3bad9bc501b
wpbullet <wpbulletplugins@gmail.com>
2018-03-04 11:29:46 -0500

    initial commit

---
+++ js/visual-editor-buttons.js
@@ -0,0 +1,48 @@
+(function() {
+    tinymce.create("tinymce.plugins.bullet_prism_button_plugin", {
+
+        //url argument holds the absolute url of our plugin directory
+        init : function(ed, url) {
+
+            //add new button     
+            ed.addButton("prism_pre", {
+                title : "Wrap in Pre + Code Tags",
+                cmd : "prism_pre_command",
+                image : url+"/p_key.png"
+            });
+            ed.addButton("prism_code", {
+                title : "Wrap in Code Tags",
+                cmd : "prism_code_command",
+                image : url+"/c_key.png"
+            });
+
+            //buttons functionality.
+            ed.addCommand("prism_pre_command", function() {
+                var selected_text = ed.selection.getContent({format : "html"});
+                var return_text = "<pre><code>" + selected_text + "</code></pre>";
+                ed.execCommand("mceInsertContent", 0, return_text);
+            });
+
+            ed.addCommand("prism_code_command", function() {
+                var selected_text = ed.selection.getContent({format : "html"});
+                var return_text = "<code>" + selected_text + "</code>";
+                ed.execCommand("mceInsertContent", 0, return_text);
+            });
+
+        },
+
+        createControl : function(n, cm) {
+            return null;
+        },
+
+        getInfo : function() {
+            return {
+                longname : "WP Bullet PrismJS Default Tag Insertion",
+                author : "Dan Dulaney",
+                version : "1"
+            };
+        }
+    });
+
+    tinymce.PluginManager.add("bullet_prism_button_plugin", tinymce.plugins.bullet_prism_button_plugin);
+})();
---
+++ wp-bullet-syntax-highlighting.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: WP Bullet Syntax Highlighting
+Plugin URI: https://dandulaney.com
+Description: Custom PrismJS integration for WP Bullet
+Version: 1.1
+Author: Dan Dulaney
+Author URI: https://dandulaney.com
+License: GPLv2
+License URI: 
+*/
+
+//enqueues all css files needed
+function bullet_prism_enqueue_style() {
+	wp_enqueue_style( 'bullet-prism-style', plugin_dir_url( __FILE__ ) . 'css/prism.css', false ); 
+	
+}
+//enqueues all js files needed
+function bullet_prism_enqueue_script() {
+	wp_enqueue_script( 'bullet-prism-js', plugin_dir_url( __FILE__ ) . 'js/prism.js', false ); 
+	wp_enqueue_style( 'dashicons' );
+}
+add_action( 'wp_enqueue_scripts', 'bullet_prism_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'bullet_prism_enqueue_script' );
+
+
+//Adds buttons to the text editor tab
+//These buttons wrap selected text in prism pre and code tags
+function bullet_prism_js_text_button_script() {
+
+    if(wp_script_is("quicktags")) {
+        ?>
+            <script type="text/javascript">
+                
+                //this function is used to retrieve the selected text from the text editor
+                function getSel() {
+
+                    var txtarea = document.getElementById("content");
+                    var start = txtarea.selectionStart;
+                    var finish = txtarea.selectionEnd;
+                    return txtarea.value.substring(start, finish);
+                }
+
+                QTags.addButton( 
+                    "prism_defaults", 
+                    "pre", 
+                    callback
+                );
+                QTags.addButton( 
+                    "prism_defaults2", 
+                    "code", 
+                    callbackCode
+                );
+
+                function callback() {
+
+                    var selected_text = getSel();
+
+	            //wraps the content with the tags
+                    QTags.insertContent("<pre><code>"+  selected_text + "</code></pre>");
+                }
+
+                function callbackCode() {
+
+                    var selected_text = getSel();
+
+	            //wraps the content with the tags
+                    QTags.insertContent("<code>"+  selected_text + "</code>");
+                }
+            </script>
+        <?php
+    }
+}
+
+add_action("admin_print_footer_scripts", "bullet_prism_js_text_button_script");
+
+function enqueue_plugin_scripts($plugin_array) {
+
+    //enqueue TinyMCE plugin script with its ID.
+    $plugin_array["bullet_prism_button_plugin"] =  plugin_dir_url(__FILE__) . "js/visual-editor-buttons.js";
+    return $plugin_array;
+}
+
+add_filter("mce_external_plugins", "enqueue_plugin_scripts");
+
+function register_buttons_editor($buttons) {
+
+    //register buttons with their id.
+    array_push($buttons, "prism_pre");
+    array_push($buttons, "prism_code");
+    return $buttons;
+}
+
+add_filter("mce_buttons", "register_buttons_editor");
